aa9c99bb00bbaa8b944a805d54827e2ff6a92fd0
Kosta <68782786+KostaD02@users.noreply.github.com>
2024-02-23 23:09:58 +0400

    feat: wrap in module

---
+++ projects/ngx-cube-loader/src/lib/ngx-cube-loader.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { NgxCubeLoaderComponent } from './ngx-cube-loader.component';
+
+@NgModule({
+  declarations: [],
+  imports: [NgxCubeLoaderComponent],
+  exports: [NgxCubeLoaderComponent],
+})
+export class NgxCubeLoaderModule {}
